14df49a800bfadaf0cf1526696bc54030f3f08a3
DoubleSpace <billy.lundh@gmail.com>
2022-12-19 11:34:30 +0100

    Update algo3.py

--- algo3.py
+++ algo3.py
@@ -2,16 +2,18 @@ import os
 
 dir_path = input("Enter the path to the directory: ")
 search_word = input("Search for a word: ")
-
-
+found = False
 
 for root, dirs, files in os.walk(dir_path):
-    
     for f in files:
         file_path = os.path.join(root, f)
         with open(file_path) as file:
             if search_word in file.read():
                 print(f"Found '{search_word}' in file {file_path}")
+                found = True
+                break
+    if found:
+        break
 
-            else:
-            	print(f"No match for keyword {search_word}")
+if not found:
+    print(f"Could't find '{search_word}' in any files")
